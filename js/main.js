5084f9f0ae381480435c76a9132e0f491ba4fab1
Cristian Castaneda <core.editor>
2021-04-12 12:31:48 +0800

    4/12/2021
    Toggle for light and dark mode.

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const insertTxtDelay = 2000;
 let textListIndex = 0;
 let charIndex = 0;
 
-function type() {
+const type = () => {
     if (charIndex < textList[textListIndex].length) {
         typedTextSpan.textContent += textList[textListIndex].charAt(charIndex);
         charIndex++;
@@ -19,34 +19,27 @@ function type() {
     }
 }
 
-function erase() {
-    if(charIndex > 0) {
+const erase = () => {
+    if (charIndex > 0) {
         typedTextSpan.textContent = textList[textListIndex].substring(0, charIndex-1);
         charIndex--;
         setTimeout(erase, removeDelay);
     } else {
         textListIndex++;
-        if(textListIndex >= textList.length) textListIndex = 0;
+        if (textListIndex >= textList.length) textListIndex = 0;
         setTimeout(type, txtDelay + 1100);
     }
 }
-
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(type, insertTxtDelay + 250);
 })
 
 
-// Light Mode
-function imgLight() {
-    let el = document.body;
-    el.classList.toggle("light-mode");
-}
-
-// Cursor
-let mouseCursor = document.querySelector('.cursor');
-
-window.addEventListener('mousemove', cursor);
-function cursor(e) {
-    mouseCursor.style.top = e.pageY + 'px';
-    mouseCursor.style.left = e.pageX + 'px';
+// For Toggle menu
+// Updated 4/12/2021
+const body = document.querySelector('body');
+const toggle = document.getElementById('toggle');
+toggle.onclick = () => {
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
 }
