85e19bee72e4e5a4864ba49a5bfb1a5150565374
Cristian Castaneda <core.editor>
2021-06-28 21:42:36 +0800

    6-28-2021
    UPDATE:
    MOBILE RESPONSIVE
    COLOR
    Text

--- js/main.js
+++ js/main.js
@@ -29,16 +29,18 @@ const sections = document.querySelectorAll('section[id]');
 
 window.addEventListener('scroll', scrollActive);
 function scrollActive() {
+
     const scrollY = window.pageYOffset;
+
     sections.foreach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50
         sectionId = current.getAttribute('id')
 
-        if (scrollY > scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.add('active')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.add('active');
         } else {
-            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.remove('active');
         }
-    })
+    });
 }
