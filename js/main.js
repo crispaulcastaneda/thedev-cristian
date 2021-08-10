c54993099524b58cba68736d339723272f0477bf
Cristian Castaneda <cristianpaul_c@yahoo.com>
2021-08-10 12:05:30 +0800

    8/10/2021
    New Design for mobile devices

--- js/main.js
+++ js/main.js
@@ -1,46 +1,64 @@
-"use strict"
+// For Text Animation - Call it for functions
+const typedTextSpan = document.querySelector('.third-highlight');
 
+const textList = ["{ Front End Developer }", "{ Web Development } ", "{ JS Developer }"];
+const txtDelay = 80;
+const removeDelay = 80;
+const insertTxtDelay = 1000;
 
-// MENU Constants
-const nav__menu = document.getElementById('nav-menu'),
-      toggle__menu = document.getElementById('nav-toggle'),
-      close__menu = document.getElementById('nav-close');
+let textListIndex = 0;
+let charIndex = 0;
 
-// SHOW
-toggle__menu.addEventListener('click', () => {
-    nav__menu.classList.toggle('show');
+function type() {
+    if (charIndex < textList[textListIndex].length) {
+        typedTextSpan.textContent += textList[textListIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, txtDelay);
+    } else {
+        setTimeout(erase, insertTxtDelay);
+    }
+}
+
+function erase() {
+    if(charIndex > 0) {
+        typedTextSpan.textContent = textList[textListIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, removeDelay);
+    } else {
+        textListIndex++;
+        if(textListIndex >= textList.length) textListIndex = 0;
+        setTimeout(type, txtDelay + 800);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(type, insertTxtDelay + 150);
 });
 
-// HIDDEN
-close__menu.addEventListener('click', () => {
-    nav__menu.classList.remove('show');
-})
 
-// REMOVE MENU
-const nav__link = document.querySelectorAll('.nav__link');
+// SCROLL TO TOP
 
-function linkAction() {
-    nav__menu.classList.remove('show')
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+var rootElement = document.documentElement
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.80) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn")
+  }
 }
-nav__link.forEach(n => n.addEventListener('click', linkAction));
 
-// SCROLL SECTIONS ACTIVE LINK
-const sections = document.querySelectorAll('section[id]');
-
-window.addEventListener('scroll', scrollActive);
-function scrollActive() {
-
-    const scrollY = window.pageYOffset;
-
-    sections.foreach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
-
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.add('active');
-        } else {
-            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.remove('active');
-        }
-    });
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
 }
+scrollToTopBtn.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll)
