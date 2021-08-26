51da14c05df4f57f2385eb5481499f7f02e8e03b
Cristian Castaneda <cristianpaul_c@yahoo.com>
2021-08-26 16:26:04 +0800

    8/25
    Changes of design

--- js/main.js
+++ js/main.js
@@ -1,64 +1,54 @@
-// For Text Animation - Call it for functions
-const typedTextSpan = document.querySelector('.third-highlight');
+(function(){
 
-const textList = ["{ Front End Developer }", "{ Web Development } ", "{ JS Developer }"];
-const txtDelay = 80;
-const removeDelay = 80;
-const insertTxtDelay = 1000;
+  var doc = document.documentElement;
+  var w = window;
 
-let textListIndex = 0;
-let charIndex = 0;
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curScroll;
+  var direction = 0;
+  var prevDirection = 0;
 
-function type() {
-    if (charIndex < textList[textListIndex].length) {
-        typedTextSpan.textContent += textList[textListIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, txtDelay);
-    } else {
-        setTimeout(erase, insertTxtDelay);
+  var header = document.getElementById('website-header');
+
+  var checkScroll = function() {
+
+    /*
+    ** Find the direction of scroll
+    ** 0 - initial, 1 - up, 2 - down
+    */
+
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) { 
+      //scrolled up
+      direction = 2;
     }
-}
-
-function erase() {
-    if(charIndex > 0) {
-        typedTextSpan.textContent = textList[textListIndex].substring(0, charIndex-1);
-        charIndex--;
-        setTimeout(erase, removeDelay);
-    } else {
-        textListIndex++;
-        if(textListIndex >= textList.length) textListIndex = 0;
-        setTimeout(type, txtDelay + 800);
+    else if (curScroll < prevScroll) { 
+      //scrolled down
+      direction = 1;
     }
-}
 
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(type, insertTxtDelay + 150);
-});
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
 
+    prevScroll = curScroll;
+  };
 
-// SCROLL TO TOP
+  var toggleHeader = function(direction, curScroll) {
+    if (direction === 2 && curScroll > 52) { 
 
-var scrollToTopBtn = document.querySelector('.scrollToTopBtn')
-var rootElement = document.documentElement
+      //replace 52 with the height of your header in px
 
-function handleScroll() {
-  // Do something on scroll
-  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-  if ((rootElement.scrollTop / scrollTotal ) > 0.80) {
-    // Show button
-    scrollToTopBtn.classList.add("showBtn")
-  } else {
-    // Hide button
-    scrollToTopBtn.classList.remove("showBtn")
-  }
-}
+      header.classList.add('hide');
+      prevDirection = direction;
+    }
+    else if (direction === 1) {
+      header.classList.remove('hide');
+      prevDirection = direction;
+    }
+  };
 
-function scrollToTop() {
-  // Scroll to top logic
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-scrollToTopBtn.addEventListener("click", scrollToTop)
-document.addEventListener("scroll", handleScroll)
+  window.addEventListener('scroll', checkScroll);
+
+})();
+
