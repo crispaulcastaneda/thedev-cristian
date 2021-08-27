efe729a7ce4bdf08b24b23a1ba106e59ec3333b9
Cristian Castaneda <cristianpaul_c@yahoo.com>
2021-08-27 15:44:54 +0800

    8/27/2021
    New design and JavaScript features
    - Vertical Tabs
    - Sound when clicked

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@
   };
 
   var toggleHeader = function(direction, curScroll) {
-    if (direction === 2 && curScroll > 52) { 
+    if (direction === 2 && curScroll > 52) {
 
       //replace 52 with the height of your header in px
 
@@ -52,3 +52,29 @@
 
 })();
 
+
+// Vertical Tabs
+function _class(name){
+  return document.getElementsByClassName(name);
+}
+
+let tabPanes = _class("tab-header")[0].getElementsByTagName("div");
+
+for(let i=0;i<tabPanes.length;i++){
+  tabPanes[i].addEventListener("click",function(){
+    _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
+    tabPanes[i].classList.add("active");
+
+    _class("tab-indicator")[0].style.top = `calc(155px + ${i*50}px)`;
+
+    _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
+    _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
+  });
+}
+
+
+// AUDIO
+function play() {
+  let audio = document.getElementById("audio");
+  audio.play();
+}
