24f1266dc236f56a6228ef6ed711cfb4a318fdc2
Cristian Castaneda <cristianpaul_c@yahoo.com>
2021-08-30 11:33:15 +0800

    8/31
    Desktop-first design, achieved.

--- js/main.js
+++ js/main.js
@@ -77,3 +77,36 @@ function play() {
   let audio = document.getElementById("audio");
   audio.play();
 }
+
+// jQuery -> SMOOTH
+window.scroll({
+  top: 2500,
+  left: 0,
+  behavior: 'smooth'
+});
+
+// SCROLL DOWN
+var scrollToTopBtn = document.querySelector('.scrollToTopBtn')
+var rootElement = document.documentElement
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.80) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn")
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll);
